c94231d8f01f01cf77a09353b782f166c20e6ae3
Aziz-Taufique <taufiqueaziz88@gmail.com>
2026-02-11 21:13:34 +0530

    setting up express server as test it

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("foo");
+import express from "express";
+
+import serverConfig from "./config/serverConfig";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.end("hello");
+});
+
+
+app.listen(serverConfig.PORT, () => {
+    console.log(`Server is running at port ${serverConfig.PORT}`);
+});
